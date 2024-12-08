b0ef0ae864568b7a2e7859e018a807a023bfb69f
Muhammad Arslan <mughalarslan996@gmail.com>
2024-12-08 12:31:34 +0500

    first vue app counter application

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+const  Increment = () => {
+  if(count.value >= 10){
+    count.value = 0
+  }
+  count.value++
+}
+
+
+const Decrement = () => {
+  if(count.value <= 0){
+    count.value = 10
+  }
+  count.value--
+}
+
+</script>
+
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="Increment">Increment</button>
+  <button @click="Decrement">Decrement</button>
+</template>
